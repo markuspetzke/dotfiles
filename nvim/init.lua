dc9ceac1e198aaa5925ee79060deeacab9244f35
Markus Petzke <markuspetzke@gmail.com>
2026-04-03 14:55:09 +0200

    added kebabcase movement to astro files

--- nvim/init.lua
+++ nvim/init.lua
@@ -7,4 +7,11 @@ require("config.options")
 
 require("config.autocmds")
 require("config.keymaps")
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "astro",
+  callback = function()
+    vim.opt_local.iskeyword:remove("-")
+  end,
+})
 -- require("bufferline").setup()
